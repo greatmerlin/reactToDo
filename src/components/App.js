b40e501083f01d9194d47f9f8f39bcac83ba3d80
Theo <gmbaxevanos@gmx.ch>
2020-09-25 23:14:08 +0200

    function to website and todo list

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import { func } from 'prop-types';
+import React from 'react';
+
+function App() {
+    return(
+        <div>
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+        </div>
+    )
+}
+
+export default App;
+
